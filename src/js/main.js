14ecbc1e2d7595155d15fcbc1ca52f9709f228dc
Arnau Mora Gras <arnyminerz@proton.me>
2023-06-01 18:16:18 +0200

    Added current tab selection storage
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- src/js/main.js
+++ src/js/main.js
@@ -4,4 +4,17 @@ window.addEventListener('load', function () {
         return document.getElementById('login_panel').classList.remove('d-none');
 
     document.getElementById('main_panel').classList.remove('d-none');
+
+    // Select tab
+    const tabIndexStr = sessionStorage.getItem('tab_index') ?? '0';
+    const tabIndex = parseInt(tabIndexStr);
+    const tabs = ['tab_events', 'tab_news', 'tab_profile'];
+    const panels = ['panel_events', 'panel_news', 'panel_profile'];
+    document.getElementById(tabs[tabIndex]).classList.add('active');
+    document.getElementById(panels[tabIndex]).classList.add('show', 'active');
+    // Add tab session storage listeners
+    tabs.map(id => document.getElementById(id))
+        .forEach((tab, index) => {
+            tab.addEventListener('click', () => sessionStorage.setItem('tab_index', index.toString()))
+        })
 });
