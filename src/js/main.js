f6bb18acbe73642e8f9873d00c44384295bc0fc2
Arnau Mora Gras <arnyminerz@proton.me>
2023-06-03 10:18:04 +0200

    Added payment methods
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- src/js/main.js
+++ src/js/main.js
@@ -33,4 +33,43 @@ window.addEventListener('load', function () {
         }
         element.innerText = buildInfo[property];
     }
+
+    for (const element of document.querySelectorAll('[data-source]')) {
+        const source = element.getAttribute('data-source');
+        const property = element.getAttribute('data-property');
+        const format = element.getAttribute('data-format') ?? 'text';
+        const listener = element.getAttribute('data-listener');
+        /** If true, all elements at the same depth as the source will be listened */
+        const listenAll = element.getAttribute('data-listen-all') ?? false;
+
+        if (!source.startsWith('#') || property == null) continue;
+        const sourceElement = document.getElementById(source.substring(1));
+        if (sourceElement == null) continue;
+
+        function updateValue() {
+            const value = sourceElement[property];
+            console.log('Updating value of', element, 'to', value, 'format:', format);
+            if (format === 'text')
+                element.innerText = value;
+            else if (format === 'display')
+                if (element.classList.contains('fade'))
+                    if (value === true)
+                        element.classList.add('show');
+                    else
+                        element.classList.remove('show');
+                else
+                    if (value === true)
+                        element.classList.remove('d-none');
+                    else
+                        element.classList.add('d-none');
+        }
+        updateValue();
+
+        if (listener != null) {
+            sourceElement.addEventListener(listener, () => updateValue());
+            if (listenAll)
+                for (let child of sourceElement.parentElement.children)
+                    child.addEventListener(listener, () => updateValue());
+        }
+    }
 });
